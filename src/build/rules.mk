97b9f499494ac56e586acd0145855cd98cba126f
zezhouliu <zezhouliu1@gmail.com>
2015-04-06 00:50:34 -0400

    Added back compile options + changed gcc to 4.7 (compatibility)

--- src/build/rules.mk
+++ src/build/rules.mk
@@ -1,4 +1,17 @@
-CC = clang
+
+GCC ?= $(shell if gcc-4.9 --version | grep gcc >/dev/null; then echo gcc-4.9; \
+	elif gcc-mp-4.7 --version | grep gcc >/dev/null; then echo gcc-mp-4.7; \
+	else echo gcc; fi 2>/dev/null)
+
+ifeq ($(PREFER_GCC),1)
+CC = $(shell if $(GCC) --version | grep gcc >/dev/null; then echo $(GCC); \
+	else echo clang; fi 2>/dev/null)
+else
+CC = $(shell if clang --version | grep LLVM >/dev/null; then echo clang; \
+	else echo $(GCC); fi 2>/dev/null)
+endif
+
+CLANG := $(shell if $(CC) --version | grep LLVM >/dev/null; then echo 1; else echo 0; fi)
 
 INCLUDE = ../include
 
