b86ce93060433b34c929c2c6e2f994248357d572
zezhouliu <zezhouliu@college.harvard.edu>
2015-04-09 13:23:33 -0600

    Added frama-c to socket-systems

--- src/socket-systems.c
+++ src/socket-systems.c
@@ -1,24 +1,61 @@
 #include "socket-systems.h"
 
+/*@
+    behavior null:
+        assumes !\valid(socket) || length <= 0 || !\valid((char*)buffer + (0..length-1));
+        assigns \nothing;
+        ensures \result == 0;
+    behavior valid_open:
+        assumes \valid(socket) && length > 0 && \valid((char*) buffer + (0..length-1));
+        assumes socket->status == SOCKET_OPEN;
+        assigns ((char*) buffer)[0..length-1];
+        ensures \result == length;
+    behavior valid_closed:
+        assumes \valid(socket) && length > 0 && \valid((char*) buffer + (0..length-1));
+        assumes socket->status != SOCKET_OPEN;
+        assigns ((char*) buffer)[0..length-1];
+        ensures \result == 0;
+
+    complete behaviors null, valid_open, valid_closed;
+    disjoint behaviors null, valid_open, valid_closed;
+*/
 ssize_t io_recv(socket_t* socket, void* buffer, size_t length, int flags)
 {
-    assert(socket);
-    assert(buffer);
-    if (socket_get_status(socket) == SOCKET_OPEN)
+    if (socket && buffer && socket_get_status(socket) == SOCKET_OPEN)
     {
         return recv (socket_get_fd(socket), buffer, length, flags);
     }
+
     return 0;
 
 }
+
+/*@
+    behavior null:
+        assumes !\valid(socket) || length <= 0 || !\valid((char*)buffer + (0..length-1));
+        assigns \nothing;
+        ensures \result == 0;
+    behavior valid_open:
+        assumes \valid(socket) && length > 0 && \valid((char*) buffer + (0..length-1));
+        assumes socket->status == SOCKET_OPEN;
+        assigns \nothing;
+        ensures \result == length;
+    behavior valid_closed:
+        assumes \valid(socket) && length > 0 && \valid((char*) buffer + (0..length-1));
+        assumes socket->status != SOCKET_OPEN;
+        assigns \nothing;
+        ensures \result == 0;
+
+    complete behaviors null, valid_open, valid_closed;
+    disjoint behaviors null, valid_open, valid_closed;
+*/
 ssize_t io_send(socket_t* socket, const void* buffer, size_t length, int flags)
 {
-    assert(socket);
-    assert(buffer);
-    if (socket_get_status(socket) == SOCKET_OPEN)
+    if (socket && buffer && socket_get_status(socket) == SOCKET_OPEN)
     {
         return send (socket_get_fd(socket), buffer, length, flags);
     }
+
     return 0;
 }
 
