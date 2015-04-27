51c37d20db2b8601c29cc49501e30e378d0ab845
zachary fogelson <fogelsonz606@gmail.com>
2015-04-27 00:09:09 +0000

    Conflicts Comin

--- http-parser/parser_symbolic_test.c
+++ http-parser/parser_symbolic_test.c
@@ -3780,10 +3780,10 @@ int valid_parser_header_state() {
             case h_content_length:
             case h_transfer_encoding:
             case h_upgrade:
-              return 0;
+              return 1;
               break;
             default:
-              return 1;
+              return 0;
               break;
   }
 }
@@ -3792,16 +3792,16 @@ int
 valid ()
 {
 	
-  if(parser_chunked_states() && !(parser->flags & F_CHUNKED)){
+  if(!(parser_chunked_states() && !(parser->flags & F_CHUNKED))){
     return 1;
   }	
-  if((parser->state == s_chunk_size_start) && !(parser->nread == 1)){
+  if(!((parser->state == s_chunk_size_start) && !(parser->nread == 1))){
     return 1; 
   }
-  if((parser->state == s_chunk_data_almost_done) && !(parser->content_length == 0)){
+  if(!((parser->state == s_chunk_data_almost_done) && !(parser->content_length == 0))){
     return 1; 
   }
-  if((parser->state == s_header_field) && !(valid_parser_header_state())){
+  if(!((parser->state == s_header_field) && !(valid_parser_header_state()))){
 	 return 1;
   }
   if(!((parser->state == s_header_value) && (parser->header_state == h_connection || parser->header_state == h_transfer_encoding))) {
