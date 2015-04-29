d304741290517ed7b0d9f56d4760f6e02cbb0862
Pulak Goyal <pulak@Administrators-MacBook-Pro-4.local>
2015-04-28 23:43:58 -0400

    added klee code to mini challenge

--- http-parser/mini_challenge/VeriServe_mini_challenge.c
+++ http-parser/mini_challenge/VeriServe_mini_challenge.c
@@ -1,6 +1,7 @@
 #include <stdio.h> 
 #include <assert.h>
 #include <string.h>
+#include <klee/klee.h>
 
 #define IS_DIGIT(d) ((d >= 48) && (d <= 57))
 
@@ -20,9 +21,22 @@ s_digit_done,
 s_fail
 }; 
 
+/* Parser struct */
+typedef struct phone_parser { 
+int s_prev; 
+int s_cur; 
+} phone_parser; 
+
+
 /* Function prototypes */
 int parse_phone_number(const char *buf, size_t buflen); 
 static enum state parse_phone_char(enum state s, const char ch); 
+int valid();
+void phone_parser_init();
+void parser_possible_state(); 
+
+// Globals 
+phone_parser * parser; 
 
 
 int
@@ -107,16 +121,109 @@ parse_phone_char(enum state s, const char ch)
   return s_fail;
 }
 
-int main(int argc, char *argv[]) { 
+// int main(int argc, char *argv[]) { 
 
-  int err = parse_phone_number(argv[1], strlen(argv[1])); 
-  if (err) { 
-    printf("Your phone number is invalid. \n"); 
-  }
-  else { 
-    printf("Your phone number is valid. \n");
-  }
+//   int err = parse_phone_number(argv[1], strlen(argv[1])); 
+//   if (err) { 
+//     printf("Your phone number is invalid. \n"); 
+//   }
+//   else { 
+//     printf("Your phone number is valid. \n");
+//   }
   
-  return 0; 
+//   return 0; 
+// }
+
+
+int valid() { 
+  if ((parser->s_prev == s_start) && !(parser->s_cur == s_digit_one || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  if ((parser->s_prev == s_digit_one) && !(parser->s_cur == s_digit_two || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  if ((parser->s_prev == s_digit_two) && !(parser->s_cur == s_digit_three || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  if ((parser->s_prev == s_digit_three) && !(parser->s_cur == s_digit_four || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  if ((parser->s_prev == s_digit_four) && !(parser->s_cur == s_digit_five || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  if ((parser->s_prev == s_digit_five) && !(parser->s_cur == s_digit_six || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  if ((parser->s_prev == s_digit_six) && !(parser->s_cur == s_digit_seven || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  if ((parser->s_prev == s_digit_seven) && !(parser->s_cur == s_digit_eight || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  if ((parser->s_prev == s_digit_eight) && !(parser->s_cur == s_digit_nine || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  if ((parser->s_prev == s_digit_nine) && !(parser->s_cur == s_digit_done || parser->s_cur == s_fail)) { 
+    return 0; 
+  }
+  return 1; 
 }
+
+int
+transition(char *buf, int len){
+  int s;
+  s = parse_phone_char(parser->s_cur, *buf); 
+  parser->s_prev = parser->s_cur; 
+  parser->s_cur = s; 
+  return valid();
+}
+
+void phone_parser_init() { 
+  klee_make_symbolic(parser, sizeof(phone_parser), "parser");
+}
+
+void parser_possible_state() { 
+klee_assume(parser->s_prev == s_start || 
+parser->s_prev == s_digit_one ||
+parser->s_prev == s_digit_two ||
+parser->s_prev == s_digit_three ||
+parser->s_prev == s_digit_four ||
+parser->s_prev == s_digit_five ||
+parser->s_prev == s_digit_six ||
+parser->s_prev == s_digit_seven ||
+parser->s_prev == s_digit_eight ||
+parser->s_prev == s_digit_nine ||
+parser->s_prev == s_digit_done ||
+parser->s_prev == s_fail); 
+
+klee_assume(parser->s_cur == s_start || 
+parser->s_cur == s_digit_one ||
+parser->s_cur == s_digit_two ||
+parser->s_cur == s_digit_three ||
+parser->s_cur == s_digit_four ||
+parser->s_cur == s_digit_five ||
+parser->s_cur == s_digit_six ||
+parser->s_cur == s_digit_seven ||
+parser->s_cur == s_digit_eight ||
+parser->s_cur == s_digit_nine ||
+parser->s_cur == s_digit_done ||
+parser->s_cur == s_fail); 
+
+}
+
+/* Solution */
+int main(int argc, char **argv) { 
+
+  phone_parser_init(); 
+  parser_possible_state();
+
+  if(valid()){
+  if(!transition(argv[1], 1)){
+        printf("%s", argv[1]);
+    klee_assert(0); 
+  } 
+  }
+}
+
+
  
